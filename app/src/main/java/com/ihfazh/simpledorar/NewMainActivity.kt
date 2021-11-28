7d8cdf242e3be290bc2a63f588e9edc3c1ad5f1d
ihfazhillah <mihfazhillah@gmail.com>
2021-11-28 17:48:01 +0700

    initiate fragments with bottom navigation

---
+++ app/src/main/java/com/ihfazh/simpledorar/NewMainActivity.kt
@@ -0,0 +1,23 @@
+package com.ihfazh.simpledorar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.ihfazh.simpledorar.databinding.ActivityNewMainBinding
+
+class NewMainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityNewMainBinding.inflate(layoutInflater).apply {
+            val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+            navController = navHost.navController
+
+            bottomNavigation.setupWithNavController(navController)
+        }
+        setContentView(binding.root)
+    }
+}
