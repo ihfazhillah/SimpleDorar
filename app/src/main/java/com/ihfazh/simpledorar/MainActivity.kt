0649eb2d9796917252a1a987c2c1af708451ac32
ihfazhillah <mihfazhillah@gmail.com>
2021-11-28 13:41:01 +0700

    convert main activity to kotlin

--- app/src/main/java/com/ihfazh/simpledorar/MainActivity.kt
+++ app/src/main/java/com/ihfazh/simpledorar/MainActivity.kt
@@ -1,92 +1,87 @@
-package com.ihfazh.simpledorar;
+package com.ihfazh.simpledorar
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.appcompat.app.AppCompatActivity
+import com.ihfazh.simpledorar.R
+import android.os.Bundle
+import android.content.Intent
+import android.app.SearchManager
+import com.google.android.material.textfield.TextInputEditText
+import android.text.Editable
+import com.ihfazh.simpledorar.AHadithResultActivity
+import com.ihfazh.simpledorar.MainActivity
+import android.speech.RecognizerIntent
+import android.app.Activity
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import java.util.*
 
-import android.app.SearchManager;
-import android.content.Intent;
-import android.os.Build;
-import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Toast;
-
-import com.google.android.material.textfield.TextInputEditText;
-
-import java.util.List;
-import java.util.Objects;
-
-public class MainActivity extends AppCompatActivity {
-    public static final String SKEY = "com.ihfazh.simpledorar.skey";
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
-        return super.onCreateOptionsMenu(menu);
+class MainActivity : AppCompatActivity() {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.menu_search){
-            onSearchRequested();
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.menu_search) {
+            onSearchRequested()
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected(item)
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(myToolbar);
-
-        Intent searchIntent = getIntent();
-        if (Intent.ACTION_SEARCH.equals(searchIntent.getAction())){
-            String skey = searchIntent.getStringExtra(SearchManager.QUERY);
-            doSearch(skey);
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val myToolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(myToolbar)
+        val searchIntent = intent
+        if (Intent.ACTION_SEARCH == searchIntent.action) {
+            val skey = searchIntent.getStringExtra(SearchManager.QUERY)
+            doSearch(skey)
         }
-
     }
 
-
-    public void onSearchClick(View view){
-        TextInputEditText textInputEditText = (TextInputEditText) findViewById(R.id.skey);
-        String skey = Objects.requireNonNull(textInputEditText.getText()).toString();
-        doSearch(skey);
+    fun onSearchClick(view: View?) {
+        val textInputEditText = findViewById<View>(R.id.skey) as TextInputEditText
+        val skey = Objects.requireNonNull(textInputEditText.text).toString()
+        doSearch(skey)
     }
 
-    private void doSearch(String skey){
+    private fun doSearch(skey: String) {
         // create intent
-        Intent intent = new Intent(this, AHadithResultActivity.class);
+        val intent = Intent(this, AHadithResultActivity::class.java)
         // send the key
         // activate the intent
-        intent.putExtra(SKEY, skey);
-        startActivity(intent);
+        intent.putExtra(SKEY, skey)
+        startActivity(intent)
     }
 
-    public void onAudioSearchClick(View view){
+    fun onAudioSearchClick(view: View?) {
         // create intent
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-SA");
-        startActivityForResult(intent, 10);
+        val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        intent.putExtra(
+            RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+            RecognizerIntent.LANGUAGE_MODEL_FREE_FORM
+        )
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "ar-SA")
+        startActivityForResult(intent, 10)
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && data != null && requestCode == 10){
-            List<String> results = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-            String skey = results.get(0);
-            doSearch(skey);
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null && requestCode == 10) {
+            val results: List<String> = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            val skey = results[0]
+            doSearch(skey)
         } else {
-            Toast.makeText(getApplicationContext(), "Failed to recognize speech!", Toast.LENGTH_LONG).show();
+            Toast.makeText(applicationContext, "Failed to recognize speech!", Toast.LENGTH_LONG)
+                .show()
         }
+    }
+
+    companion object {
+        const val SKEY = "com.ihfazh.simpledorar.skey"
     }
 }
